79a28789f89684e98e717a5751a94d21688cfa63
Dan Carlos <dancarlos22@gmail.com>
2022-10-20 18:49:34 -0300

    Ajustes

--- src/main/java/loja/springboot/controller/EstadoController.java
+++ src/main/java/loja/springboot/controller/EstadoController.java
@@ -1,5 +1,7 @@
 package loja.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import loja.springboot.model.Estado;
+import loja.springboot.model.Pessoa;
 import loja.springboot.repository.EstadoRepository;
 
 @Controller
@@ -40,28 +44,36 @@ public class EstadoController {
 
 	
 	
+	@RequestMapping(method = RequestMethod.GET, value = "cadastroestado")
+	public ModelAndView cadastro(Estado estado) {
+		ModelAndView modelAndView = new ModelAndView("estado/cadastroestado");
+		modelAndView.addObject("estadobj", new Estado());
+		return modelAndView;
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST, value ="salvarestado")
+	public ModelAndView salvar(Estado estado) {
+		ModelAndView andView = new ModelAndView("estado/cadastroestado");
+		andView.addObject("estadobj",estadoRepository.saveAndFlush(estado));
+		return andView;
+
+	}
+	
+	
 	@GetMapping("/editarestado/{idestado}")
 	public ModelAndView editar(@PathVariable("idestado") Long idestado) {
-		
-		//	Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
-
-		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
-		//	modelAndView.addObject("pessoas", pessoaRepository.findAll());
-	//	modelAndView.addObject("pessoaobj", pessoa.get());
-		return modelAndView;
-		
+		Optional<Estado> estado = estadoRepository.findById(idestado);
+		return salvar(estado.get());
 	}
 	
 	
 	@GetMapping("/removerestado/{idestado}")
 	public ModelAndView excluir(@PathVariable("idestado") Long idestado) {
-		
-		//pessoaRepository.deleteById(idpessoa);	
-		
-		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
-	//	modelAndView.addObject("pessoas", pessoaRepository.findAll());
-	//	modelAndView.addObject("pessoaobj", new Pessoa());
-		return modelAndView;
+		estadoRepository.deleteById(idestado);	
+		ModelAndView andView = new ModelAndView("estado/lista");
+		andView.addObject("estados", estadoRepository.top10());
+		return andView;
 		
 	}
 	
--- src/main/java/loja/springboot/repository/EstadoRepository.java
+++ src/main/java/loja/springboot/repository/EstadoRepository.java
@@ -2,15 +2,15 @@ package loja.springboot.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import loja.springboot.model.Estado;
 @Repository
 @Transactional
-public interface EstadoRepository extends CrudRepository<Estado, Long> {
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
 	@Query("select p from Estado p where UPPER(p.nome) like %?1% OR UPPER(p.sigla) like %?1%")
 	List<Estado> findEstadoByName(String nome);
 	
