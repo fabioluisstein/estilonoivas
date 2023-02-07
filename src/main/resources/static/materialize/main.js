7844e1b3bab7785ca4ec5ec0a08482b376a85bda
Dan Carlos <dancarlos22@gmail.com>
2023-02-07 17:19:18 -0300

    novo

--- src/main/resources/static/materialize/main.js
+++ src/main/resources/static/materialize/main.js
@@ -141,9 +141,50 @@ const instanceTooltip = M.Tooltip.init(elemsTooltip, {
 });
 
 
+
+/* Máscaras de cpf/cnpj */
+    document.getElementById('cpf_cnpj').addEventListener('input', function(e) {
+    
+        let x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
+        e.target.value = !x[2] ? x[1] : x[1] + '.' + x[2] + (x[3] ? '.' : '') + x[3] + (x[4] ? '/' : x[4]) + x[4] + (x[5] ? '-' + x[5] : '');
+        
+        if(e.target.value.length < 15) {
+            x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
+            e.target.value = !x[2] ? x[1] : x[1] + '.' + x[2] + (x[3] ? '.' : '') + x[3] + (x[4] ? '-' + x[4] : '');
+        }
+
+    });
+
+
+
+/* Máscaras de Telefones */
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,""); //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('telefone').onkeyup = function(){
+		mascara( this, mtel );
+	}
+}
+
 $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+
 
 
 $("#valorCompra").maskMoney({showSymbol:true, symbol:"R$ ", decimal:",", thousands:"."});
@@ -167,6 +208,7 @@ $("#valorVenda").maskMoney({showSymbol:true, symbol:"R$ ", decimal:",", thousand
 $("#valor").val(formatter.format($("#valor").val()));
 $("#valor").focus();
 $("#valor").maskMoney({showSymbol:true, symbol:"R$ ", decimal:",", thousands:"."});
+
 
 
 $("#valorProduto").val(formatter.format($("#valorProduto").val()));
