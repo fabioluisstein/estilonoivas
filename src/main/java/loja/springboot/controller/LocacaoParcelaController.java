1e3913aba027b718b51df60c7f2bbdb0f10105f2
Dan Carlos <dancarlos22@gmail.com>
2023-03-30 13:44:12 -0300

    novo commit

--- src/main/java/loja/springboot/controller/LocacaoParcelaController.java
+++ src/main/java/loja/springboot/controller/LocacaoParcelaController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/loja/springboot/repository/CidadeRepository.java
+++ src/main/java/loja/springboot/repository/CidadeRepository.java
@@ -1,12 +1,9 @@
 package loja.springboot.repository;
-
 import java.util.List;
-
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
-
 import loja.springboot.model.Cidade;
 
 @Transactional
@@ -15,7 +12,7 @@ public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 	InterfaceGeneric interfaceGeneric = new InterfaceGeneric();
 
 	@Cacheable("cidadesTodas") 
-	@Query(value = "select  id,   nome,  estado,  estado_id , quantidadeCliente   from vw_dataTable_cidades  order by nome asc ", nativeQuery = true)
+	@Query(value = "select  id,   nome,  estado,  estado_id , quantidadeCliente   from vw_datatable_cidades  order by nome asc ", nativeQuery = true)
 	List<listCidades> listCidadades();
 	public static interface listCidades {
 		String getId(); 
